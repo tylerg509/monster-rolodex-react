006df14f7e7fdf55e971715bd32d67ba7b34d1cc
Tyler <gasperlint@gmail.com>
2019-11-27 17:36:28 -0600

    Updates

---
+++ precache-manifest.ca9b59b1a1ad285894744d33ff276f09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be70dc50906da727819c01ef60fd01b6",
+    "url": "/monster-rolodex-react/index.html"
+  },
+  {
+    "revision": "d24f760990771e6cbbe4",
+    "url": "/monster-rolodex-react/static/css/main.50291688.chunk.css"
+  },
+  {
+    "revision": "2ea260805c2d8eeb767a",
+    "url": "/monster-rolodex-react/static/js/2.bce2a7e5.chunk.js"
+  },
+  {
+    "revision": "d24f760990771e6cbbe4",
+    "url": "/monster-rolodex-react/static/js/main.a5d86f83.chunk.js"
+  },
+  {
+    "revision": "3407c20f90077d26d4cc",
+    "url": "/monster-rolodex-react/static/js/runtime-main.3fba59db.js"
+  }
+]);
