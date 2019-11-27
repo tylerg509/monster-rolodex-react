0e9bc7a2f1b1afc259be38e60d9e612f088b270b
Tyler <gasperlint@gmail.com>
2019-11-27 02:41:04 -0600

    add monster images

--- src/App.js
+++ src/App.js
@@ -23,8 +23,7 @@ class  App extends Component {
 render(){
   return(
     <div className="App">
-      <CardList name="TJG">
-         { this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)}
+      <CardList monsters={this.state.monsters }>
 
       </CardList>
     </div>
